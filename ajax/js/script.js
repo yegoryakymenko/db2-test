7070b36d48cb6f727ae5226f610bb57944cd95b4
Yegor Yakimenko <intheskyp@gmail.com>
2018-11-11 16:33:00 +0200

    Adding buttons for species

--- ajax/js/script.js
+++ ajax/js/script.js
@@ -1,10 +1,9 @@
-let urlApiPeople = 'https://swapi.co/api/people/';
-let urlApiPlanets = 'https://swapi.co/api/planets/';
-let urlApiFilms = 'https://swapi.co/api/films/';
-let urlApiSpecies = 'https://swapi.co/api/species/';
-let urlApiVehicles = 'https://swapi.co/api/vehicles/';
-let urlApiStarships = 'https://swapi.co/api/starships/';
-
+const urlApiPeople = 'https://swapi.co/api/people/';
+const urlApiPlanets = 'https://swapi.co/api/planets/';
+const urlApiFilms = 'https://swapi.co/api/films/';
+const urlApiSpecies = 'https://swapi.co/api/species/';
+const urlApiVehicles = 'https://swapi.co/api/vehicles/';
+const urlApiStarships = 'https://swapi.co/api/starships/';
 
 let btn = document.querySelectorAll('.button');
 let people = document.querySelector('.people');
@@ -16,7 +15,8 @@ let starships = document.querySelector('.starships');
 let answerField = document.querySelector('.answer-field');
 let ul = document.querySelector('ul');
 let next = document.querySelector('.next');
-let previous = document.querySelector('.previous');         
+let previous = document.querySelector('.previous');
+let sort = document.querySelector('.sort');         
 
 people.addEventListener('click', function() {
   getInfo(urlApiPeople);
@@ -42,33 +42,56 @@ next.addEventListener('click', function() {
 previous.addEventListener('click', function() {
   getInfo(previous);
 });
+sort.addEventListener('click', function() {
+
+})
 
 function getInfo(url) {
   let xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
+   xhr.open('GET', url, true);
   xhr.onreadystatechange = function(){
         if(xhr.readyState == 4 && xhr.status == 200){
             let response = JSON.parse(xhr.responseText);
             let result = response.results;
+            console.log(result);
             next = response.next;
             previous = response.previous;
-            if (ul.childNodes.length) {
-                ul.innerHTML = "";
-              }
+            ul.innerHTML = "";
             result.forEach(info => {
+              console.log(info);
               let li = document.createElement('li');
+              let btnShowMore = document.createElement('button');
+              let liProp = document.createElement('li');
+              let ulProp = document.createElement('ul');
+              btnShowMore.innerHTML = 'Show more';
               if (url == urlApiFilms) {
               li.innerHTML = info.title;
             } else {
               li.innerHTML = info.name;
+              liProp.innerHTML = `Height: ${info.height} <br> Mass: ${info.mass} <br> Birth year ${info.birth_year}`;
             }
-              
               ul.appendChild(li);
-            })
+              ul.appendChild(btnShowMore);
+               ulProp.appendChild(liProp);
+                li.appendChild(ulProp);
 
+              btnShowMore.addEventListener('click', function() {
+                ulProp.classList.toggle('active');
+                
+               
+              })
+            })
+             
         }
     }
+ 
   xhr.send();
 
 }
-
+// Сделать функцию в котрой в переменную будет записываться текущий name
+// и по нему выводить инфу по
+// 1. Клик по кнопке
+//  2. После клик аппендиться список со свойствами, при повторном клике убирается
+//  Otdaenlp  -  в спсок изначально вложить список и сделать ему display: none;
+//
+//
